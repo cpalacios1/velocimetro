0a89a376173c687d6308c4233821bfdbaafef097
Christian <christian@toracom.net>
2016-02-19 18:19:57 -0600

    Initial Commit

---
+++ Velocímetro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocímetro
+//
+//  Created by toracom Desarrollador Cuatro on 2/19/16.
+//  Copyright © 2016 toracom Desarrollador Cuatro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
